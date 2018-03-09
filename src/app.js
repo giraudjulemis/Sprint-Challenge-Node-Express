0046bdfce00e8d96165302682091b66efc33168b
Giraud Julemis <giraud.julemis@gmail.com>
2018-03-09 14:02:03 -0500

    created current price and yesterday price

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
 const express = require('express');
+const fetch = require('node-fetch');
 
 const app = express();
 
-const port = 3000;
+const PORT = 3000;
+const STATUS_SUCCESS = 200;
+const STATUS_ERROR = 422;
+
+const CURRENT_PRICE = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+const YESTERDAY_PRICE = 'https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday';
 
 const router = express.Router();
 
 router.route('/compare')
   .get((req, res) => {
-    res.json({ hello: 'This is compare route' });
+    const currentprice = fetch(CURRENT_PRICE)
+      .then(res => res.json())
+      .then(rate => rate.bpi.USD.rate_float)
+      .then(result => {
+        return result;
+      })
+      .catch(err => {
+        res.status(STATUS_ERROR);
+        res.send({ error: err });
+      });
+
+    const yesterdayprice = fetch(YESTERDAY_PRICE)
+      .then(res => res.json())
+      .then(rate => rate.bpi);
   });
 
 app.use('/api', router);
@@ -17,6 +36,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to my API');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on PORT: ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
 });
